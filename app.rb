c518782bf5c20343086e1285ca92d3718be0a3a4
soccert <talyagriffin2004@gmail.com>
2018-08-01 17:57:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,10 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/food_form' do
-    erb :food_form
+  get '/xportx' do
+    erb :xportx
   end
 
   # Add your post route and action below
-
+post '/sport' do
+  @name=params[:name]
+  @favorite_sport=params[:sport]
+  erb :me
+  end
 end
